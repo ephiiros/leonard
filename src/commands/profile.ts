17119ddb52e0113e659c5ceb073a22b54dbc5cae
ephiros <iliakoskos@gmail.com>
2025-05-08 11:06:57 +0200

    profile

--- src/commands/profile.ts
+++ src/commands/profile.ts
@@ -27,11 +27,13 @@ export async function execute(interaction: CommandInteraction) {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   // boxes
-  ctx.fillStyle = "#dedede";
+  ctx.fillStyle = "#b2b2b2";
   ctx.beginPath();
   ctx.roundRect(15, 15, canvas.width - 30, 50, 5);
   ctx.roundRect(15, 75, 130, 130, 5);
   ctx.roundRect(155, 75, 130, 130, 5);
+  ctx.roundRect(15, 215, 130, 130, 5);
+  ctx.roundRect(15, 360, 265, 130, 5);
   ctx.fill();
 
   // avatar
@@ -43,13 +45,16 @@ export async function execute(interaction: CommandInteraction) {
   // check if user exists
   return pb
     .collection("User" + interaction.user.id)
-    .getFullList()
+    .getFullList({
+      sort: "-DateTime_UTC"
+    })
     .then(async (historyList) => {
       logger.info(`${JSON.stringify(historyList)}`);
       let totalPoints = 0;
       let totalWins = 0;
       let totalScoreWins = 0;
       let maxWins = 0;
+      let blobOffset = 20
       for (var historyItem of historyList) {
         totalPoints += parseInt(historyItem.PointsRecieved);
         if (historyItem.PointsRecieved !== "0") {
@@ -62,6 +67,25 @@ export async function execute(interaction: CommandInteraction) {
           totalScoreWins += 1;
         }
         maxWins += 1;
+
+        if (
+          historyItem.Picked ===
+          historyItem.Team1Score + "-" + historyItem.Team2Score
+        ) {
+          //green
+          ctx.fillStyle = "#64e3a1";
+        } else if (historyItem.PointsRecieved === "0") {
+          //red
+          ctx.fillStyle = "#ff7f7f";
+        } else {
+          //yellow
+          ctx.fillStyle = "#e3cc64";
+        }
+        
+        ctx.beginPath()
+        ctx.roundRect(blobOffset, 450 - parseInt(historyItem.PointsRecieved) * 10, 10, 10, 2);
+        ctx.fill()
+        blobOffset += 12
       }
 
       const winrate = Math.round(totalWins / maxWins * 100);
@@ -71,9 +95,12 @@ export async function execute(interaction: CommandInteraction) {
       ctx.fillText(`${interaction.user.username}`, 70, 55);
       ctx.fillText(`Winrate%`, 20, 100);
       ctx.fillText(`Score WR%`, 160, 100);
+      ctx.fillText(`Total Points`, 20, 240);
+      ctx.fillText(`LP graph lol`, 20, 390);
       ctx.font = "50px Roboto";
       ctx.fillText(`${winrate}%`, 40, 160);
       ctx.fillText(`${scoreWinrate}%`, 180, 160);
+      ctx.fillText(`${totalPoints}`, 40, 300);
 
       const attachment = new AttachmentBuilder(canvas.toBuffer("image/png"), {
         name: "profile-image.png",
